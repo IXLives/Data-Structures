ce1271358559a9941931e4c43912a7a3df59b840
Ian Bryant <48700036+IXLives@users.noreply.github.com>
2020-03-18 20:30:27 -0400

    Lecture code stuff

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -17,17 +17,16 @@ class BinarySearchTree:
         # if greater go right
         # if no node to go to (left or right)
         # make new node at that spot
-        curNode = self.value
-        if curNode > value:
+        if self.value <= value:
             if self.right is None:
-                self.right = value
+                self.right = BinarySearchTree(value)
             else:
-                curNode = self.right
-        if curNode < value:
+                self.right.insert(value)
+        if self.value > value:
             if self.left is None:
-                self.left = value
+                self.left = BinarySearchTree(value)
             else:
-                curNode = self.left
+                self.left.insert(value)
     # Return True if the tree contains the value
     # False if it does not
 
@@ -41,16 +40,35 @@ class BinarySearchTree:
         # return false
         # if larger but no right
         # return false
-        pass
+
+        if self.value < target:
+            if self.right is None:
+                return False
+            else:
+                return self.right.contains(target)
+        if self.value > target:
+            if self.left is None:
+                return False
+            else:
+                return self.left.contains(target)
+        if self.value == target:
+            return True
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        # maximum value should be the rightmost value on the tree
+        if self.right is None:
+            return self.value
+        return self.right.get_max()
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        cb(self.value)
+        if self.left:
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
 
     # DAY 2 Project -----------------------
 
